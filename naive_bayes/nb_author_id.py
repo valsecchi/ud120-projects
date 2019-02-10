1706e90bce9505ae16f304dfa5c216dbe0f2d0a2
Jacopo Valsecchi <Jacopo.Valsecchi@etu.unige.ch>
2019-02-10 15:47:50 +0100

    Getting Started with Mini-Projects: Naive Bayes Classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,18 +16,16 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+prd = clf.predict(features_test)
 
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print("Accuracy:", accuracy_score(labels_test, prd))
